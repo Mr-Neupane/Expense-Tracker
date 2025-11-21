c126227bd30f802528f78b033c35d775a2e548ff
Mr-Neupane <nischal.prathamit@gmail.com>
2025-11-21 09:26:01 +0545

    feat: bank transaction handling

---
+++ ExpenseTracker/Controllers/BankTransactionController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using ExpenseTracker.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using TestApplication.ViewModels;
+
+namespace ExpenseTracker.Controllers;
+
+public class BankTransactionController : Controller
+{
+    [HttpGet]
+    public IActionResult BankDepositandWithdraw()
+    {
+        return View();
+    }
+    
+   [HttpPost]
+    public async Task<IActionResult> BankDepositandWithdraw(BankTransactionVm vm)
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public JsonResult GetBanks()
+    {
+        var dbConnection = DapperConnectionProvider.GetConnection();
+        string query = "SELECT id, bankname FROM bank.bank"; 
+        var banks = dbConnection.Query(query).Select(b => new 
+        {
+            Id = b.id,
+            bankname = b.bankname
+        }).ToList();
+        return Json(banks);
+    }
+}
---
+++ ExpenseTracker/ViewModels/BankTransactionVm.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.ViewModels;
+
+public class BankTransactionVm
+{
+  public string BankName { get; set; }
+  public DateOnly TxnDate { get; set; }
+  public decimal Amount { get; set; }
+  public string Remarks { get; set; }
+  public string Type { get; set; }
+}
