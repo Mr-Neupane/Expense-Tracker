10faee91661475309e91d23d3a61db8bcb067755
Mr-Neupane <nischal.prathamit@gmail.com>
2025-12-15 09:24:23 +0545

    feat: wip on Ledger Statement

--- ExpenseTracker/ViewModels/LedgerstatementVm.cs
+++ ExpenseTracker/ViewModels/LedgerstatementVm.cs
@@ -5,4 +5,13 @@ public class LedgerstatementVm
     public DateTime DateFrom { get; set; }
     public DateTime DateTo { get; set; }
     public int LedgerId { get; set; }
+    public List<LedgerStatement> LedgerStatements { get; set; }
+}
+
+public class LedgerStatement
+{
+    public string LedgerName { get; set; }
+    public decimal DrAmount { get; set; }
+    public decimal CrAmount { get; set; }
+    public DateTime TxnDate { get; set; }
 }
