aa3c7fb5b6cbef082e3c0adc48e3e89858cad3f6
Nischal061 <neupanenischal517@gmail.com>
2026-01-28 18:40:18 +0545

    add transaction scope while recording transaction.

--- ExpenseTracker/Manager/TransactionManager.cs
+++ ExpenseTracker/Manager/TransactionManager.cs
@@ -1,10 +1,11 @@
-﻿using ExpenseTracker.Dtos;
-using ExpenseTracker.Models;
+﻿using System.Transactions;
+using ExpenseTracker.Dtos;
 using ExpenseTracker.Providers;
 using TestApplication.Interface;
 using TestApplication.ViewModels.Interface;
+using Transaction = ExpenseTracker.Models.Transaction;
 
-namespace TestApplication.Manager;
+namespace ExpenseTracker.Manager;
 
 public class AccTransactionManager
 {
@@ -28,7 +29,7 @@ public class AccTransactionManager
 
     private async Task<Transaction> RecordVoucher(AccTransactionDto accTransaction, int typeId)
     {
-        var acctxn = await _voucherService.RecordTransactionAsync(new AccTransactionDto
+        var accTxn = await _voucherService.RecordTransactionAsync(new AccTransactionDto
         {
             TxnDate = accTransaction.TxnDate,
             Amount = accTransaction.Amount,
@@ -38,89 +39,108 @@ public class AccTransactionManager
             IsJv = false,
             Details = accTransaction.Details,
         });
-        return acctxn;
+        return accTxn;
     }
 
     public async Task RecordBankTransaction(BankTransactionDto dto, AccTransactionDto accTransaction)
     {
-        var banktransaction = new BankTransactionDto
+        using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
         {
-            BankId = dto.BankId,
-            TxnDate = dto.TxnDate,
-            Amount = dto.Amount,
-            Type = dto.Type,
-            Remarks = dto.Remarks,
-        };
-        var banktxn = await _bankService.RecordBankTransactionAsync(banktransaction);
-        var acctxn = await RecordVoucher(accTransaction, banktxn.Id);
-        await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(banktxn.Id, acctxn.Id);
-        await _bankService.UpdateRemainingBalanceInBankAsync(dto.BankId);
+            var bankTransaction = new BankTransactionDto
+            {
+                BankId = dto.BankId,
+                TxnDate = dto.TxnDate,
+                Amount = dto.Amount,
+                Type = dto.Type,
+                Remarks = dto.Remarks,
+            };
+            var bankTxn = await _bankService.RecordBankTransactionAsync(bankTransaction);
+            var accTxn = await RecordVoucher(accTransaction, bankTxn.Id);
+            await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(bankTxn.Id, accTxn.Id);
+            await _bankService.UpdateRemainingBalanceInBankAsync(dto.BankId);
+            scope.Complete();
+        }
     }
 
 
-    public async Task RecordExpenseTransaction(NewExpenseDto dto, AccTransactionDto txndto)
+    public async Task RecordExpenseTransaction(NewExpenseDto dto, AccTransactionDto txnDto)
     {
-        var expense = await _expenseService.RecordExpenseAsync(dto);
-
-        var accTrans = await RecordVoucher(txndto, expense.Id);
-        var bankid = await _provider.GetBankIdByLedgerId(dto.FromLedgerId);
-        if (bankid != 0)
+        using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
         {
-            var bankTransaction = await _bankService.RecordBankTransactionAsync(new BankTransactionDto
+            var expense = await _expenseService.RecordExpenseAsync(dto);
+
+            var accTrans = await RecordVoucher(txnDto, expense.Id);
+            var bankId = await _provider.GetBankIdByLedgerId(dto.FromLedgerId);
+            if (bankId != 0)
             {
-                BankId = bankid,
-                TxnDate = dto.TxnDate,
-                Amount = dto.Amount,
-                Type = "Withdraw",
-                Remarks = txndto.Remarks,
-            });
-            await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(bankTransaction.Id,
-                accTrans.Id);
-            await _bankService.UpdateRemainingBalanceInBankAsync(bankTransaction.BankId);
+                var bankTransaction = await _bankService.RecordBankTransactionAsync(new BankTransactionDto
+                {
+                    BankId = bankId ?? 0,
+                    TxnDate = dto.TxnDate,
+                    Amount = dto.Amount,
+                    Type = "Withdraw",
+                    Remarks = txnDto.Remarks,
+                });
+                await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(bankTransaction.Id,
+                    accTrans.Id);
+                await _bankService.UpdateRemainingBalanceInBankAsync(bankTransaction.BankId);
+                
+            }
+            scope.Complete();
         }
     }
 
     public async Task RecordIncomeTransaction(IncomeDto idto, AccTransactionDto dto)
     {
-        var income = await _incomeService.RecordIncomeAsync(idto);
-        var transaction = await RecordVoucher(dto, income.Id);
-
-
-        int bankid = await _provider.GetBankIdByLedgerId(idto.FromLedgerid);
-        if (bankid != 0)
+        using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
         {
-            var bankTransaction = new BankTransactionDto
+            var income = await _incomeService.RecordIncomeAsync(idto);
+            var transaction = await RecordVoucher(dto, income.Id);
+
+
+            var bankId = await _provider.GetBankIdByLedgerId(idto.FromLedgerid);
+            if (bankId != 0)
             {
-                BankId = bankid,
-                TxnDate = dto.TxnDate.ToLocalTime(),
-                Amount = dto.Amount,
-                Type = "Deposit",
-                Remarks = dto.Remarks
-            };
-            await _bankService.RecordBankTransactionAsync(bankTransaction);
-            await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(bankTransaction.Id,
-                transaction.Id);
+                var bankTransaction = new BankTransactionDto
+                {
+                    BankId = bankId ?? 0,
+                    TxnDate = dto.TxnDate.ToLocalTime(),
+                    Amount = dto.Amount,
+                    Type = "Deposit",
+                    Remarks = dto.Remarks
+                };
+                await _bankService.RecordBankTransactionAsync(bankTransaction);
+                await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(bankTransaction.Id,
+                    transaction.Id);
+               
+            }
+            scope.Complete();
         }
     }
 
-    public async Task RecordLiabilityTransaction(LiabilityDto lidto, AccTransactionDto txndto)
+    public async Task RecordLiabilityTransaction(LiabilityDto liDto, AccTransactionDto txnDto)
     {
-        var liability = await _liabilityService.RecordLiabilityAsync(lidto);
-        var accTransaction = await RecordVoucher(txndto, liability.Id);
-
-        if (lidto.BankId != 0)
+        using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
         {
-            var banktransaction = await _bankService.RecordBankTransactionAsync(new BankTransactionDto
+            var liability = await _liabilityService.RecordLiabilityAsync(liDto);
+            var accTransaction = await RecordVoucher(txnDto, liability.Id);
+
+            if (liDto.BankId != 0)
             {
-                BankId = lidto.BankId,
-                LedgerId = 0,
-                TxnDate = lidto.TxnDate.ToLocalTime(),
-                Amount = lidto.Amount,
-                Type = "Deposit",
-                Remarks = lidto.Remarks,
-            });
-            await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(banktransaction.Id,
-                accTransaction.Id);
+                var bankTransaction = await _bankService.RecordBankTransactionAsync(new BankTransactionDto
+                {
+                    BankId = liDto.BankId,
+                    LedgerId = 0,
+                    TxnDate = liDto.TxnDate.ToLocalTime(),
+                    Amount = liDto.Amount,
+                    Type = "Deposit",
+                    Remarks = liDto.Remarks,
+                });
+                await _bankService.UpdateAccountingTransactionIdInBankTransactionAsync(bankTransaction.Id,
+                    accTransaction.Id);
+            }
+
+            scope.Complete();
         }
     }
 }
